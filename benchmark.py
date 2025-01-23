38ae8149603e6e6fda1a95100e9bfc810a534e1d
Karim Ibrahim <Karim.Ibrahim@ibm.com>
2025-01-23 22:33:08 +0100

    adjust benchmark

--- benchmark.py
+++ benchmark.py
@@ -137,69 +137,69 @@ def simulation_fixed_vectors():
 # Simulation with random vectors of length m and n where the values of the vectors are between 1 and p and k is fixed
 def simulation_fixed_k():
     results = []
-    x_max = p_order
-    y_max = p_order
-    F_max = p_order
-    for k in range(3, 4):
-        m = k
-        n = k - 1
-        x = random_vector(1, x_max, n)
-        y = random_vector(1, y_max, m)
-        F = random_int_matrix(1, F_max, n, m)
-        p = p_order
+    x_max = 2
+    y_max = 2
+    F_max = 2
+    k = 200
+    m = k
+    n = k - 1
+    x = random_vector(1, x_max, n)
+    y = random_vector(1, y_max, m)
+    F = random_int_matrix(1, F_max, n, m)
+    p = p_order
 
-        start_time = time.time()
-        mpk, msk = G.setup(p, k)
-        setup_time = time.time() - start_time
+    start_time = time.time()
+    mpk, msk = G.setup(p, k)
+    setup_time = time.time() - start_time
 
-        start_time = time.time()
-        skF = G.keygen(p, mpk, msk, F)
-        keygen_time = time.time() - start_time
+    start_time = time.time()
+    skF = G.keygen(p, mpk, msk, F)
+    keygen_time = time.time() - start_time
 
-        start_time = time.time()
-        CT_xy = G.encrypt(msk, x, y)
-        encrypt_time = time.time() - start_time
+    start_time = time.time()
+    CT_xy = G.encrypt(msk, x, y)
+    encrypt_time = time.time() - start_time
 
-        start_time = time.time()
-        v = G.decrypt(p, mpk, skF, CT_xy, n, m, F)
-        decrypt_time = time.time() - start_time
+    start_time = time.time()
+    v = G.decrypt(p, mpk, skF, CT_xy, n, m, F)
+    decrypt_time = time.time() - start_time
 
-        setup_time *= 1_000_000_000
-        keygen_time *= 1_000_000_000
-        encrypt_time *= 1_000_000_000
-        decrypt_time *= 1_000_000_000
-        total_time = setup_time + keygen_time + encrypt_time + decrypt_time
+    setup_time *= 1_000_000_000
+    keygen_time *= 1_000_000_000
+    encrypt_time *= 1_000_000_000
+    decrypt_time *= 1_000_000_000
+    total_time = setup_time + keygen_time + encrypt_time + decrypt_time
 
-        s_msk = size_in_kilobits(msk)
-        s_mpk = size_in_kilobits(mpk)
-        s_ct = size_in_kilobits(CT_xy)
-        s_sk = size_in_kilobits(skF)
+    s_msk = size_in_kilobits(msk)
+    s_mpk = size_in_kilobits(mpk)
+    s_ct = size_in_kilobits(CT_xy)
+    s_sk = size_in_kilobits(skF)
 
-        expected = G.get_expected_result(p, x, F, y)
-        print("expected result: ", expected)
-        print("calculated result: ", v)
+    expected = G.get_expected_result(p, x, F, y)
+    print("expected result: ", expected)
+    print("calculated result: ", v)
 
-        results.append(
-            [
-                k,
-                m,
-                n,
-                x_max,
-                y_max,
-                F_max,
-                s_msk,
-                s_mpk,
-                s_ct,
-                s_sk,
-                setup_time,
-                keygen_time,
-                encrypt_time,
-                decrypt_time,
-                total_time,
-            ]
-        )
+    results.append(
+        [
+            k,
+            m,
+            n,
+            x_max,
+            y_max,
+            F_max,
+            s_msk,
+            s_mpk,
+            s_ct,
+            s_sk,
+            setup_time,
+            keygen_time,
+            encrypt_time,
+            decrypt_time,
+            total_time,
+        ]
+    )
 
-    with open("data/qfe_benchmark_fixed_k.csv", "w", newline="") as csvfile:
+    with open("data/qfe_benchmark_p_vectors_200", "w", newline="") as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(
             [
@@ -223,6 +223,6 @@ def simulation_fixed_k():
         csvwriter.writerows(results)
 
 
-simulation_fixed_vectors()
-#simulation_fixed_k()
+#simulation_fixed_vectors()
+simulation_fixed_k()
 #implementation_check()
